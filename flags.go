e17b68ad657148681df86022ae22caab9f4c5129
Luciano Ramalho <luciano@ramalho.org>
2015-05-21 01:29:25 -0300

    working on flags.go

--- flags.go
+++ flags.go
@@ -13,22 +13,28 @@ const POP20_CC = "CN IN US ID BR PK NG BD RU JP MX PH VN ET EG DE IR TR CD FR"
 
 const BASE_URL = "http://flupy.org/data/flags"
 
+const DEST_DIR = "downloads/"
+
 func check(e error) {
 	if e != nil {
 		panic(e)
 	}
 }
 
+func save_flag(img []byte, filename string) {
+	path := fmt.Sprintf("%v%v", DEST_DIR, filename)
+	fmt.Println(path)
+}
+
 func get_flag(cc string) {
 	url := fmt.Sprintf("%[1]v/%[2]v/%[2]v.gif", BASE_URL, strings.ToLower(cc))
 	fmt.Println(cc, url)
 	resp, err := http.Get(url)
-	if err != nil {
-		fmt.Println(url, err)
-	}
+	check(err)
 	defer resp.Body.Close()
 	body, err := ioutil.ReadAll(resp.Body)
 	fmt.Println(string(body[:6]), resp.ContentLength)
+	save_flag(body, fmt.Sprintf("%v.gif", cc))
 }
 
 func main() {
